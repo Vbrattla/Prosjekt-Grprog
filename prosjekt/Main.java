16dfd18c8ec34fa97e4c341cf8869649ccb0b3b2
Vegard Brattland <V.Brattland@stud.uis.no>
2011-12-27 18:46:42 +0100

    Første

--- prosjekt/Main.java
+++ prosjekt/Main.java
@@ -3,6 +3,9 @@ package prosjekt;
 public class Main {
 
     public static void main(String[] args) {
-        
+  
+        /*Test av GITHUB
+         * Kommer det forandring??
+         */
     }
 }
